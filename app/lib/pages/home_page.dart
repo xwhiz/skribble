6e0e65adcc20e1b1e28f010294153231088b803a
aqsa-batool <aqsabatool2005@gmail.com>
2025-05-09 01:07:18 +0500

    feat: update home page and show temporarily round page before game screen

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -1,20 +1,17 @@
 import 'dart:ui';
-
+import 'package:app/pages/round_page.dart';
 import 'package:provider/provider.dart';
 import 'package:app/pages/create_room.dart';
-import 'package:app/pages/exisitng_rooms.dart';
 import 'package:app/pages/game_layout.dart';
 import 'package:app/pages/join_private_room.dart';
 import 'package:flutter/material.dart';
 import 'package:app/viewmodels/matchmaking_view_model.dart';
-import 'package:app/logger.dart'; // same import
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     //Access the ViewModel
     final viewModel = Provider.of<MatchmakingViewModel>(context);
 
@@ -52,27 +49,44 @@ class HomePage extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       ElevatedButton(
-                        onPressed: viewModel.isLoading ? null : () async {
-                          print("Hello");
-                          // log.info("Joining public game...");
-                          await viewModel.joinRoom();
-                                
-                            // Check if joining was successful
-                            if (viewModel.room != null && viewModel.error == null) {
-                              Navigator.push(
-                                // ignore: use_build_context_synchronously
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const GameLayout(),
-                                ),
-                              );
-                            } else if (viewModel.error != null) {
-                              // Show error message
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text(viewModel.error!)),
-                              );
-                            }
-                        },
+                        onPressed:
+                            viewModel.isLoading
+                                ? null
+                                : () async {
+                                  print("Hello");
+                                  // log.info("Joining public game...");
+                                  await viewModel.joinRoom();
+
+                                  // Check if joining was successful
+                                  if (viewModel.room != null &&
+                                      viewModel.error == null) {
+                                    // Show RoundPage temporarily, then navigate to GameLayout
+                                    Navigator.push(
+                                      // ignore: use_build_context_synchronously
+                                      context,
+                                      MaterialPageRoute(
+                                        builder:
+                                            (_) => RoundPage(
+                                              onFinish: () {
+                                                Navigator.pushReplacement(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                    builder:
+                                                        (_) =>
+                                                            const GameLayout(),
+                                                  ),
+                                                );
+                                              },
+                                            ),
+                                      ),
+                                    );
+                                  } else if (viewModel.error != null) {
+                                    // Show error message
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(content: Text(viewModel.error!)),
+                                    );
+                                  }
+                                },
                         style: ElevatedButton.styleFrom(
                           // ignore: deprecated_member_use
                           backgroundColor: Colors.white,
@@ -82,31 +96,31 @@ class HomePage extends StatelessWidget {
                           padding: const EdgeInsets.symmetric(
                             horizontal: 50,
                             vertical: 15,
-                          )
-                          ),
-                      
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(
-                                Icons.public,
-                                color: Color.fromARGB(179, 32, 42, 53),
-                              ),
-                              const SizedBox(width: 10),
-                              const Text(
-                                "Join Public Game",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  color: Color.fromARGB(179, 32, 42, 53),
-                                  fontWeight: FontWeight.normal,
-                                  fontFamily:
-                                      'ComicNeue', // Applying Comic Neue font
-                                ),
-                              ),
-                            ],
                           ),
                         ),
-                  
+
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.public,
+                              color: Color.fromARGB(179, 32, 42, 53),
+                            ),
+                            const SizedBox(width: 10),
+                            const Text(
+                              "Join Public Game",
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Color.fromARGB(179, 32, 42, 53),
+                                fontWeight: FontWeight.normal,
+                                fontFamily:
+                                    'ComicNeue', // Applying Comic Neue font
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
                       const SizedBox(height: 10),
                       SizedBox(
                         width: 300,
